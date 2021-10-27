659090e7379ba8d83e01651dfbe6a3a2758dd3b0
Rudra1227 <89596109+Rudra1227@users.noreply.github.com>
2021-10-26 21:21:46 -0500

    computation.js

---
+++ routes/computation.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+//var value=Math.random;
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    fake_url = "https://fake.com/path" + req.url
+    const url = new URL(fake_url)
+    const search_params = url.searchParams
+    if (req.method === 'GET') {
+        value = search_params.get("x");
+        if(value === null)
+        {
+            value=Math.round(Math.random()*900);
+        }
+        console.log(value);
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('Math.abs() applied to ' + value + ' is ' + Math.abs(value))
+        res.write('<br/>Math.acos() applied to ' + value + ' is ' + Math.acos(value))
+        res.write('<br/>Math.sin() applied to ' + value + ' is ' + Math.sin(value))
+        res.write('<br/>Math.sinh() applied to ' + value + ' is ' + Math.sinh(value))
+        res.end()
+    }
+});
+
+module.exports = router;
